9de94a054469a751190baacd0043eb9f490cc0c4
Milko <milko@local.com>
2024-06-20 13:19:49 +0000

    Added followers DB and Model

--- database/migrations/2024_06_20_130003_followers.php
+++ database/migrations/2024_06_20_130003_followers.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('followers', function (Blueprint $table) {
+            $table->foreignId("follower_id")->constrained('users')->onDelete('cascade');
+            $table->foreignId("followee_id")->constrained('users')->onDelete('cascade');
+            $table->primary(['follower_id', 'followee_id']);
+        });
     }
 
     /**
@@ -19,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('followers');
     }
 };
